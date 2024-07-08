f4cd7aa02084ed0fcad0eb06e408e1927c4cc55d
Utopia5327 <mb5327@columbia.edu>
2024-07-08 19:47:22 -0400

    change index.html

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,1 +1,14 @@
 console.log("this works");
+
+console.log("Hello, World!");
+const API_KEY = config.WEATHER_API_KEY;
+
+
+
+  function getWeatherData() {
+    // fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${185d820a2b1ee524ac272628e3f1ea7b}`)
+  then(response => response.json())
+  then(data => console.log(data));
+  }
+  
+  getWeatherData()
